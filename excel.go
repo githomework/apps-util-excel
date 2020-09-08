e8a0cf869a9bf218ec1dfbecba6e827ae7208129
David Lam <david.lam@winc.com.au>
2020-09-08 18:45:50 +1000

    commit

--- excel.go
+++ excel.go
@@ -4,8 +4,10 @@ import (
 	"time"
 )
 
+const layout = "2006-01-02 15:04:05"
+
 // Force time to UTC to work with another Excel package.
 func TimeToUTC(t time.Time) time.Time {
-	t, _ = time.ParseInLocation("2006-01-02 15:04", t.Format("2006-01-02 15:04"), time.UTC)
+	t, _ = time.ParseInLocation(layout, t.Format(layout), time.UTC)
 	return t
 }
